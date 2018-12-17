da88a3916e1acf04c9b2323c5ef027940cd232a6
steffen <steffen@ravenclaw>
2018-12-17 21:56:53 +0100

    Add maze playing notebook

--- Dev/maze.py
+++ Dev/maze.py
@@ -6,9 +6,11 @@ from game import Game
 actions = {0:'left', 1:'right', 2:'up', '3':'down'}
 
 class Maze(Game):
-    def __init__(self, grid_size=10):
-        self.grid_size=10
-    
+    def __init__(self):
+        self.width = 5
+        self.height = 5
+        self.reset()
+        
     @property
     def name(self):
         return "Maze"
@@ -17,20 +19,50 @@ class Maze(Game):
         return 4
     
     def reset(self):
-        pass
-    
+        self.grid = [[0 for i in range(self.width)] for j in range(self.height)]
+        barriers = ((0,2),(2,1),(2,2),(2,3),(2,4),(4,0),(4,3))
+        for bx,by in barriers:
+            self.grid[bx][by] = 1
+        self.grid[0][0] = 2
+        self.grid[4][4] = 3
+        self.x, self.y = 0,4
+        self.bumped = False
+        self.fallen = False
+        self.won = False
+
     def play(self, action):
-        pass
-    
+        if self.fallen or self.won:
+            return
+        dx,dy = ((-1,0),(1,0),(0,-1),(0,1))[action]
+        nx,ny = self.x+dx, self.y+dy
+        self.bumped = False
+        self.fallen = False
+        self.won = False
+        if (nx < 0) or (nx>=self.width) or (ny < 0) or (ny >= self.height):
+            self.bumped = True
+        elif self.grid[nx][ny] == 1:
+            self.bumped = True
+        elif self.grid[nx][ny] == 2:
+            self.fallen = True
+        elif self.grid[nx][ny] == 3:
+            self.won = True
+        if not self.bumped:
+            self.x, self.y = nx,ny
+
     def get_score(self):
-        pass
+        if self.fallen: return -1
+        if self.won: return 1
+        if self.bumped: return -0.5
+        return -0.1
     
     def get_state(self):
-        pass
+        canvas = np.asarray(self.grid).astype('float32')
+        canvas[self.x][self.y] = 4.0
+        return canvas/4.0
     
     def is_over(self):
-        pass
+        return self.fallen or self.won
 
     def is_won(self):
-        pass
+        return self.won
 
