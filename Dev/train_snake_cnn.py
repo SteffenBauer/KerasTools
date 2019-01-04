c1deed0def5ce4aaf433febe15914b5465a6f2cd
steffen <steffen@ravenclaw>
2019-01-04 22:27:27 +0100

    Cleanup

--- Dev/train_snake_cnn.py
+++ Dev/train_snake_cnn.py
@@ -6,14 +6,14 @@ import KerasTools as KT
 import numpy as np
 
 grid_size = 10
-nb_frames = 1
+nb_frames = 5
 
 model = keras.models.Sequential()
-model.add(keras.layers.Reshape((grid_size, grid_size, 3), input_shape=(1, grid_size, grid_size, 3)))
+model.add(keras.layers.Permute((2,3,1), input_shape=(nb_frames, grid_size, grid_size)))
 model.add(keras.layers.Conv2D(16, (3, 3), activation='relu'))
 model.add(keras.layers.Conv2D(32, (3, 3), activation='relu'))
 model.add(keras.layers.Flatten())
-model.add(keras.layers.Dense(64, activation='relu'))
+model.add(keras.layers.Dense(16, activation='relu', kernel_regularizer=keras.regularizers.l2(0.01)))
 model.add(keras.layers.Dense(3, activation='linear'))
 model.compile(keras.optimizers.rmsprop(), "logcosh")
 model.summary()
@@ -22,5 +22,5 @@ game = KT.qlearn.snake.Snake(grid_size, max_turn=64)
 agent = KT.qlearn.agent.Agent(model=model, memory_size=65536, nb_frames = nb_frames)
 
 agent.train(game, batch_size=256, epochs=100, train_interval=128,
-            epsilon=[0.5, 0.01], epsilon_rate=0.1, gamma=0.8, reset_memory=False)
+            epsilon=[0.5, 0.0], epsilon_rate=0.1, gamma=0.8, reset_memory=False)
 
