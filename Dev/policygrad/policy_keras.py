c43627b78516676e170a07a1f7a78f819077e041
SteffenBauer <sbauer@arago.de>
2019-08-23 16:39:02 +0200

    [WIP] Keras policy gradient RL batch training (not working yet)

--- Dev/policygrad/policy_keras.py
+++ Dev/policygrad/policy_keras.py
@@ -15,27 +15,23 @@ learning_rate = 0.0009
 
 def generate_model():
     input_state = keras.layers.Input(shape=(l1,), name="Input_State")
-    input_dr    = keras.layers.Input(shape=[1], name="Input_Discounted_Rewards")
     x = keras.layers.Dense(l2)(input_state)
     x = keras.layers.LeakyReLU()(x)
     actions = keras.layers.Dense(l3, activation='softmax')(x)
     
     def loss_fn(y_true, y_pred): 
-        return keras.backend.mean(input_dr * keras.backend.log(y_true-y_pred))
+        return keras.backend.sum(y_true * keras.backend.log(y_pred))
 
-    model_train = keras.models.Model(inputs=[input_state, input_dr], outputs=actions)
-    model_train.compile(loss=loss_fn, optimizer=keras.optimizers.Adam(learning_rate))
-    model_predict = keras.models.Model(inputs=[input_state], outputs=actions)
-    return model_train, model_predict
+    model = keras.models.Model(inputs=input_state, outputs=actions)
+    model.compile(loss=loss_fn, optimizer=keras.optimizers.Adam(learning_rate))
 
-model_train, model_predict = generate_model()
-model_train.summary()
-model_predict.summary()
+    return model
 
-exit(1)
+model = generate_model()
+model.summary()
 
 MAX_DUR = 20
-MAX_EPISODES = 100
+MAX_EPISODES = 10000
 gamma_ = 0.95
 time_steps = []
 
@@ -51,7 +47,7 @@ for episode in range(MAX_EPISODES):
     transitions = [] # list of state, action, rewards
     
     for t in range(MAX_DUR): #while in episode
-        act_prob = model_predict.predict(np.expand_dims(np.asarray(curr_state), axis=0))
+        act_prob = model.predict(np.expand_dims(np.asarray(curr_state), axis=0))
         action = np.random.choice(np.array([0,1,2]), p=act_prob[0])
         prev_state = curr_state
         curr_state, reward, done = env.play(action)
@@ -63,25 +59,24 @@ for episode in range(MAX_EPISODES):
 
     # Optimize policy network with full episode
     ep_len = len(transitions) # episode length
-    time_steps.append(ep_len)
-    preds = np.zeros(ep_len)
-    discounted_rewards = np.zeros(ep_len)
+    discounted_rewards = np.zeros((ep_len, l3))
+    train_states = []
     for i in range(ep_len): #for each step in episode
-        discount = 1
-        future_reward = 0
+        discount = 1.0
+        future_reward = 1e-8
         # discount rewards
         for i2 in range(i, ep_len):
             future_reward += transitions[i2][2] * discount
             discount = discount * gamma_
-        discounted_rewards[i] = future_reward
-        state, action, _ = transitions[i]
-        pred = model_predict.predict(np.expand_dims(np.asarray(state), axis=0))
-        preds[i] = pred[0][action]
-
+        discounted_rewards[i,:] = [future_reward]*l3
+        train_states.append(transitions[i][0])
+    train_states = np.asarray(train_states)
     # Backpropagate model with preds & discounted_rewards here
-    model_train.train_on_batch([state, discounted_rewards], preds)
+    loss = model.train_on_batch(train_states, discounted_rewards)
+    loss_stats.append(loss)
     
-    if len(win_stats) >= 10:
-        print("Episode {: 4d} Win perc {:2.4f}".format(episode, sum(win_stats)/10.0))
+    if len(win_stats) >= 100:
+        print("Episode {: 4d} Win perc {:2.4f} Loss {:2.4f}".format(episode, sum(win_stats)/100.0, sum(loss_stats)/100.0))
         win_stats = []
-
+        loss_stats = []
+        
