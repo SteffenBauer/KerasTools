10b7ae6634708cb1678a5edc38d86c703748872d
SteffenBauer <sbauer@arago.de>
2020-04-07 09:57:11 +0200

    RL: Add timestamp to history callback entries

--- Dev/rl/callbacks/history.py
+++ Dev/rl/callbacks/history.py
@@ -8,9 +8,11 @@ class HistoryLog(Callback):
         self.timestamp = "{:04d}{:02d}{:02d}_{:02d}{:02d}{:02d}".format(st.tm_year, st.tm_mon, st.tm_mday, st.tm_hour, st.tm_min, st.tm_sec)
         self.filename = '{}-{}.log'.format(name, self.timestamp)
         with open(self.filename, 'w+') as fp:
-            fp.write('Epoch, Epsilon,    Loss, Win Ratio, Avg Score, Max Score,   Memory\n')
+            fp.write('Epoch, Epsilon,    Loss, Win Ratio, Avg Score, Max Score,   Memory,      Timestamp\n')
     def epoch_end(self, *args):
         _model, name, epoch, epsilon, loss, win_ratio, avg_score, max_score, memory = args
+        st = time.gmtime()
+        timestamp = "{:04d}-{:02d}-{:02d}_{:02d}-{:02d}-{:02d}".format(st.tm_year, st.tm_mon, st.tm_mday, st.tm_hour, st.tm_min, st.tm_sec)
         with open(self.filename, 'a') as fp:
-            fp.write('{:> 5d}, {:>7.2f}, {:>7.4f}, {:>9.2%}, {:>9.2f}, {:>9.2f}, {:>8d}\n'.format(epoch, epsilon, loss, win_ratio, avg_score, max_score, memory))
+            fp.write('{:> 5d}, {:>7.2f}, {:>7.4f}, {:>9.2%}, {:>9.2f}, {:>9.2f}, {:>8d}, {}\n'.format(epoch, epsilon, loss, win_ratio, avg_score, max_score, memory, timestamp))
 
--- Dev/rl/train_catch.py
+++ Dev/rl/train_catch.py
@@ -17,22 +17,21 @@ nb_frames = 1
 game = catch.Catch(grid_size)
 
 inp = keras.layers.Input(shape=(nb_frames, grid_size, grid_size, 3))
-x = keras.layers.Conv3D(32,3,padding='same',strides=1,activation='relu')(inp)
-x = keras.layers.Conv3D(64,3,padding='same',strides=2,activation='relu')(x)
+x = keras.layers.Conv3D(16,5,padding='same',strides=1,activation='relu')(inp)
 x = keras.layers.Flatten()(x)
-x = keras.layers.Dense(128, activation='relu')(x)
+x = keras.layers.Dense(32, activation='relu')(x)
 act = keras.layers.Dense(game.nb_actions, activation='linear')(x)
 
 model = keras.models.Model(inputs=inp, outputs=act)
-model.compile(keras.optimizers.adam(), 'logcosh')
+model.compile(keras.optimizers.rmsprop(), 'logcosh')
 model.summary()
 
-m = uniqmemory.UniqMemory(memory_size=4096)
+m = uniqmemory.UniqMemory(memory_size=8192)
 a = dqn.Agent(model=model, mem=m, num_frames = nb_frames)
 history = history.HistoryLog("catch")
 
-a.train(game, batch_size=32, epochs=50, episodes=256,
-            epsilon=[0.5, 0.0], epsilon_rate=0.1,
-            gamma=0.95, reset_memory=False, observe=256, verbose=1,
+a.train(game, batch_size=32, epochs=20, episodes=256, train_interval=32,
+            epsilon=[0.0, 0.0], epsilon_rate=0.25,
+            gamma=0.95, reset_memory=False, observe=128, verbose=1,
             callbacks = [history])
 
